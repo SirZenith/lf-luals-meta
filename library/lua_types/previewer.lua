7079a9be62f2c0971cbb4ab8f8e4c1bc2e145be9
陆离 <sirzenith@163.com>
2026-06-09 13:40:08 +0800

    add preview cleaner definition

--- library/lua_types/previewer.lua
+++ library/lua_types/previewer.lua
@@ -1,7 +1,9 @@
+---@alias lf.PreviewerCleanFunc fun(prev_file: string, w: integer, h: integer, x: integer, y: integer, next_file: string): string?
 ---@alias lf.PreviewerAction fun(buf_writer: lf.bufio.BufWriter, path: string, w: integer, h: integer, x: integer, y: integer, mode: string): boolean?, string?
 
 ---@class lf.Previewer
 ---@field condition? fun(path: string): boolean?
+---@field clean? lf.PreviewerCleanFunc
 ---@field action lf.PreviewerAction
 ---@field is_sync? boolean
 
