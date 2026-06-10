a03ca6fd582959d2312d0c2619de71ac14a8650b
陆离 <sirzenith@163.com>
2026-06-10 20:26:30 +0800

    refine: simplify lf_types signature definition

--- library/globals.lua
+++ library/globals.lua
@@ -1,31 +1,31 @@
 ---@meta
 
-lf_types = {}
-
--- app
-lf_types.App = nil ---@type lf.App
--- complete
-lf_types.CompMatch = nil ---@type lf.CompMatch
--- exec
-lf_types.Cmd = nil ---@type lf.exec.Cmd
--- misc
-lf_types.BufWriter = nil ---@type lf.bufio.BufWriter
-lf_types.BufReader = nil ---@type lf.bufio.BufReader
-lf_types.FileInfo = nil ---@type lf.fs.FileInfo
--- nav
-lf_types.File = nil ---@type lf.File
-lf_types.Dir = nil ---@type lf.Dir
-lf_types.Nav = nil ---@type lf.Nav
--- tcell
-lf_types.TcellColor = nil ---@type lf.tcell.Color
-lf_types.TcellStyle = nil ---@type lf.tcell.Style
--- time
-lf_types.Time = nil ---@type lf.time.Time
-lf_types.Month = nil ---@type lf.time.Time
-lf_types.Weekday = nil ---@type lf.time.Weekday
-lf_types.Duration = nil ---@type lf.time.Weekday
--- ui
-lf_types.UI = nil ---@type lf.UI
+lf_types = {
+    -- app
+    App = nil, ---@type lf.App
+    -- complete
+    CompMatch = nil, ---@type lf.CompMatch
+    -- exec
+    Cmd = nil, ---@type lf.exec.Cmd
+    -- misc
+    BufWriter = nil, ---@type lf.bufio.BufWriter
+    BufReader = nil, ---@type lf.bufio.BufReader
+    FileInfo = nil, ---@type lf.fs.FileInfo
+    -- nav
+    File = nil, ---@type lf.File
+    Dir = nil, ---@type lf.Dir
+    Nav = nil, ---@type lf.Nav
+    -- tcell
+    TcellColor = nil, ---@type lf.tcell.Color
+    TcellStyle = nil, ---@type lf.tcell.Style
+    -- time
+    Time = nil, ---@type lf.time.Time
+    Month = nil, ---@type lf.time.Time
+    Weekday = nil, ---@type lf.time.Weekday
+    Duration = nil, ---@type lf.time.Weekday
+    -- ui
+    UI = nil, ---@type lf.UI
+}
 
 ---@type lf.App
 app = nil
