dab9ac5b67dcfa71799458efd52ce78efa8af6e7
陆离 <sirzenith@163.com>
2026-06-13 03:33:20 +0800

    add API for IconDef, IconMap

---
+++ library/bindings/main/icon_def.lua
@@ -0,0 +1,13 @@
+---@meta
+
+---@class lf.IconDef
+local IconDef = {}
+
+---@return string?
+function IconDef:icon() end
+
+---@return boolean
+function IconDef:has_style() end
+
+---@return lf.tcell.Style
+function IconDef:style() end
---
+++ library/bindings/main/icon_map.lua
@@ -0,0 +1,8 @@
+---@meta
+
+---@class lf.IconMap
+local IconMap = {}
+
+---@param file lf.File
+---@return lf.IconDef
+function IconMap:get(file) end
