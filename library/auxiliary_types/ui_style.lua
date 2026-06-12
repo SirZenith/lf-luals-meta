4ce65f632fc6321bed152bb8fdfcf678ec4225f0
陆离 <sirzenith@163.com>
2026-06-12 19:35:53 +0800

    add optional mark to lf.UIStyleTbl fields

--- library/auxiliary_types/ui_style.lua
+++ library/auxiliary_types/ui_style.lua
@@ -4,11 +4,11 @@
 ---@alias lf.UIStyleEntry lf.tcell.Style | lf.UIStyleFunc
 
 ---@class lf.UIStyleTbl
----@field border lf.UIStyleEntry
----@field copy lf.UIStyleEntry
----@field cut lf.UIStyleEntry
----@field menu lf.UIStyleEntry
----@field menuheader lf.UIStyleEntry
----@field menuselect lf.UIStyleEntry
----@field select lf.UIStyleEntry
----@field visual lf.UIStyleEntry
+---@field border? lf.UIStyleEntry
+---@field copy? lf.UIStyleEntry
+---@field cut? lf.UIStyleEntry
+---@field menu? lf.UIStyleEntry
+---@field menuheader? lf.UIStyleEntry
+---@field menuselect? lf.UIStyleEntry
+---@field select? lf.UIStyleEntry
+---@field visual? lf.UIStyleEntry
