0759bb17d95ce95b8ee92615545932098da96af2
陆离 <sirzenith@163.com>
2026-06-17 14:08:41 +0800

    update sort method definition

--- library/auxiliary_types/registry_table/sorting_method.lua
+++ library/auxiliary_types/registry_table/sorting_method.lua
@@ -1,6 +1,14 @@
 ---@meta
 
----@alias lf.SortingMethodFunc fun(list: lf.File[]): lf.File[]
+---@class lf.SortingOptions
+---@field dircounts boolean
+---@field dirfirst boolean
+---@field dironly boolean
+---@field hidden boolean
+---@field sortignorecase boolean
+---@field sortignoredia boolean
+
+---@alias lf.SortingMethodFunc fun(f1: lf.File, f2: lf.File, options: lf.SortingOptions): integer
 
 ---@class lf.SortingMethod
 ---@field action lf.SortingMethodFunc
